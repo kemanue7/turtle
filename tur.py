3634d70cc579717d5bb6039ebe6d2544127fdce6
HE31-bit <herickcoelhodasilva@gmail.com>
2025-08-12 15:45:51 -0300

    Update tur.py

--- tur.py
+++ tur.py
@@ -4,15 +4,15 @@ from turtle import Turtle
 # Classe base Figura
 class Figura:
     def __init__(self, cor, tamanho, x, y):
-        self.c = cor                  # Cor da forma
-        self.tamanho = tamanho        # Tamanho da forma
+        self.c = cor                  # Cor 
+        self.tamanho = tamanho        # Tamanho
         self.x = x                    # Posição X
         self.y = y                    # Posição Y
-        self.t = turtle.Turtle()      # Objeto turtle para desenho
+        self.t = turtle.Turtle()     
         self.t.color(cor)
 
     def desenhar(self):
-        pass  # Método genérico a ser sobrescrito nas subclasses
+        pass  
 
 
 # Função do desenho do círculo
@@ -66,6 +66,7 @@ def menu():
     print('2 - Triângulo')
     print('3 - Quadrado')
     print('4 - Hexágono')
+    print('5- Todas as formas geometricas')
 
 
 # Função de escolha da forma geométrica
@@ -107,6 +108,39 @@ elif escolha == '4':
     y = float(input('Qual a posição y: '))
     figura = hexagono(cor, tamanho, x, y)
     figura.desenhar()
+
+elif escolha == '5':
+    print('Círculo')
+    cor1 = input('Qual a cor: ')
+    tamanho1 = float(input('Qual o tamanho: '))
+    x1 = float(input('Qual a posição x: '))
+    y1 = float(input('Qual a posição y: '))
+    figura = circulo(cor1, tamanho1, x1, y1)
+    figura.desenhar()
+    print('Triângulo')
+    cor2 = input('Qual a cor: ')
+    tamanho2 = float(input('Qual o tamanho: '))
+    x2 = float(input('Qual a posição x: '))
+    y2 = float(input('Qual a posição y: '))
+    figura = triangulo(cor2, tamanho2, x2, y2)
+    figura.desenhar()
+    print('Quadrado')
+    cor3 = input('Qual a cor: ')
+    tamanho3 = float(input('Qual o tamanho: '))
+    x3 = float(input('Qual a posição x: '))
+    y3 = float(input('Qual a posição y: '))
+    figura = quadrado(cor3, tamanho3, x3, y3)
+    figura.desenhar()
+    print('Hexágono')
+    cor4 = input('Qual a cor: ')
+    tamanho4 = float(input('Qual o tamanho: '))
+    x4 = float(input('Qual a posição x: '))
+    y4 = float(input('Qual a posição y: '))
+    figura = hexagono(cor4, tamanho4, x4, y4
+                      )
+    figura.desenhar()
+
+    
 
 else:
     print('Opção inválida.')
